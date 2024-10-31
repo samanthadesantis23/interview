a98987d1cb5b37d7186cbd2a837374058009e3f3
samanthadesantis23 <samdesantis23@gmail.com>
2024-10-31 11:25:01 -0400

    Add files via upload

---
+++ interview.py
@@ -0,0 +1,10 @@
+# Get input from the user
+number = float(input("Enter a number: "))
+
+# Conditional statements to check the number
+if number > 0:
+    print("The number is positive.")
+elif number < 0:
+    print("The number is negative.")
+else:
+    print("The number is zero.")
